e33f12697d4aca0f17353ed8133cb7c7422b89d4
YnnamTenob <mbonet@earthlink.net>
2015-11-21 14:46:50 -0500

    Commiting Programming assignment 2. Git is the most obtuse and unneccesarily complicated source control I have ever used.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Accept some matrix 'x' as the argument to make another   
+## object that stores a cached value of the matrix 
+## defined by the setCache function
+makeCacheMatrix <- function(m = matrix()) {
+  cache <- NULL
+  set <- function(y) {
+    m <<- y
+    cache <<- NULL
+  }
+  get <- function() m
+  setCache <- function(s) cache <<- s
+  getCache <- function() cache
+  list(set = set, 
+       get = get,
+       setCache = setCache,
+       getCache = getCache)
 }
 
 
-## Write a short comment describing this function
-
+## if the cache is empty 
+## set the cache to the inverse of some Cache Matrix object  
+## as defined by the makeCacheMatrix function, 
+## otherwise get the cached inverse and return the inverse 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  i <- x$getCache()
+  if(!is.null(i)) {
+    message("getting cached data")
+  }
+  else
+  {
+    i <- solve(x$get(), ...)
+    x$setCache(i)
+  }
+  i
 }
